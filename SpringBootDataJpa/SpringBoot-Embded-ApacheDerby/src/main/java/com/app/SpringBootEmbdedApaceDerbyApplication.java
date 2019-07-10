63cc20f6ab7404322c07f8044e81e371ff3b1a9d
Incredible Srv <saurabh.vaish1993@gmail.com>
2019-07-10 12:16:13 +0530

    SpringBoot-Embded-ApacheDerby Example

---
+++ SpringBootDataJpa/SpringBoot-Embded-ApacheDerby/src/main/java/com/app/SpringBootEmbdedApaceDerbyApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEmbdedApaceDerbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEmbdedApaceDerbyApplication.class, args);
+	}
+
+}
---
+++ SpringBootDataJpa/SpringBoot-Embded-ApacheDerby/src/main/java/com/app/runner/ProductRunner.java
@@ -0,0 +1,35 @@
+package com.app.runner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.app.model.Product;
+import com.app.repository.ProductRepository;
+
+// here implementation class of ProductRepository (I) will be created at runtime and its object also
+// Proxy - the class which is created at runtime and its object also and destroy at runtime only , no .class file created 
+
+
+@Component
+public class ProductRunner implements CommandLineRunner {
+
+	@Autowired
+	private ProductRepository repo;
+	
+	@Override
+	public void run(String... args) throws Exception {
+			
+		System.out.println(repo.getClass().getName());   // o/p -  com.sun.proxy.$Proxy78
+		
+		Product p =	repo.save(new Product("abc",2.2));
+		System.out.println(p.getProdId());
+		
+		repo.save(new Product("xy",3.3));
+		repo.save(new Product("pqr",4.4));
+		
+		repo.findAll().forEach(System.out::println);
+		System.out.println("done");
+	}
+
+}
