07591762dcd0e9ff622e66a86d531b83a38015ef
Incredible Srv <saurabh.vaish1993@gmail.com>
2019-07-20 15:41:55 +0530

    Spring Boot Batch - Inrtro app

---
+++ SpringBoot-Batch-IntroApp/src/main/java/com/app/SpringBootBatchIntroAppApplication.java
@@ -0,0 +1,18 @@
+package com.app;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+
+@SpringBootApplication
+@EnableBatchProcessing							// to enable batch processing
+public class SpringBootBatchIntroAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBatchIntroAppApplication.class, args);
+	}
+
+}
